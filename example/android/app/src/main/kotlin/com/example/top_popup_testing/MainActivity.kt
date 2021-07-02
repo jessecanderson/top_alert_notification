68c5b6cb9afeb5869ce7bdde45c178f2d35c60fe
jesecanderson <jesse.c.anderson@icloud.com>
2021-07-02 13:28:48 -0400

    moved over example app

---
+++ example/android/app/src/main/kotlin/com/example/top_popup_testing/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.top_popup_testing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:top_alert_notification/top_alert_notification.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeView(),
+    );
+  }
+}
+
+class HomeView extends StatelessWidget {
+  final String url = "https://docs.google.com/forms/d/1OTlXehum7L5ZKGFZ9yphOGmfs_OInIG90Ixsc_F30AE/edit";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Spacer(),
+          Center(
+            child: ElevatedButton(
+              child: Text("Press Me"),
+              onPressed: () => {
+                TopAlertNotification(
+                  context,
+                  Text(
+                    "Please take a short feedback survey",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                  closeOnX: true,
+                  onTap: printTest,
+                  color: const Color(0xFFF79912),
+                ).showTopAlert()
+              },
+            ),
+          ),
+          Spacer(),
+        ],
+      ),
+    );
+  }
+
+  void printTest() async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    }
+  }
+}
